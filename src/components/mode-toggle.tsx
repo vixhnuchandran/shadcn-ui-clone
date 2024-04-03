ef743dadb409d5787f83a158df66285c3a4d396c
vixhnuchandran <vixhnuchandran@gmail.com>
2024-04-03 16:41:30 +0530

    Add colors to the button

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -16,8 +16,8 @@ export function ModeToggle() {
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <Sun className="h-[1.2rem] w-[1.2rem] text-yellow-500 rotate-0 scale-100 transition-all dark:text-yellow-400 dark:rotate-90 dark:scale-0" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] text-indigo-500 rotate-90 scale-0 transition-all dark:text-indigo-400 dark:rotate-0 dark:scale-100" />
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
