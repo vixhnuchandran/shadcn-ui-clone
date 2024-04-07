1c678b9e3a0628ef8bdbbe2f4cd6f8f643731ce5
vixhnuchandran <vixhnuchandran@gmail.com>
2024-04-08 05:09:06 +0530

    Fix imports

--- src/components/mail/MailList.tsx
+++ src/components/mail/MailList.tsx
@@ -3,24 +3,27 @@ import { formatDistanceToNow } from "date-fns/formatDistanceToNow"
 import { cn } from "@/lib/utils"
 import { Badge } from "@/components/ui/badge"
 import { ScrollArea } from "@/components/ui/scroll-area"
-import { Mail } from "@/components/mail/data"
+import { MyMail } from "@/components/mail/data"
 import { useMail } from "@/components/mail/use-mail"
 
 interface MailListProps {
-    items: Mail[]
+    items: MyMail[]
+    viewPort?: string
 }
 
-export function MailList({ items }: MailListProps) {
+export function MailList({ items, viewPort }: MailListProps) {
     const [mail, setMail] = useMail()
 
     return (
-        <ScrollArea className="h-screen">
+        <ScrollArea className={viewPort === "mobile" ? " h-52" : "h-screen"}>
             <div className="flex flex-col gap-2 p-4 pt-0">
                 {items.map(item => (
                     <button
                         key={item.id}
                         className={cn(
-                            "flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent",
+                            `flex flex-col items-start ${
+                                viewPort === "mobile" ? "gap-0" : "gap-2"
+                            } rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent`,
                             mail.selected === item.id && "bg-muted"
                         )}
                         onClick={() =>
@@ -57,23 +60,26 @@ export function MailList({ items }: MailListProps) {
                                 {item.subject}
                             </div>
                         </div>
-                        <div className="line-clamp-2 text-xs text-muted-foreground">
-                            {item.text.substring(0, 300)}
-                        </div>
-                        {item.labels.length ? (
-                            <div className="flex items-center gap-2">
-                                {item.labels.map(label => (
-                                    <Badge
-                                        key={label}
-                                        variant={getBadgeVariantFromLabel(
-                                            label
-                                        )}
-                                    >
-                                        {label}
-                                    </Badge>
-                                ))}
+                        {viewPort !== "mobile" && (
+                            <div className="line-clamp-2 text-xs text-muted-foreground">
+                                {item.text.substring(0, 300)}
                             </div>
-                        ) : null}
+                        )}
+                        {viewPort !== "mobile" &&
+                            (item.labels.length ? (
+                                <div className="flex items-center gap-2">
+                                    {item.labels.map(label => (
+                                        <Badge
+                                            key={label}
+                                            variant={getBadgeVariantFromLabel(
+                                                label
+                                            )}
+                                        >
+                                            {label}
+                                        </Badge>
+                                    ))}
+                                </div>
+                            ) : null)}
                     </button>
                 ))}
             </div>
