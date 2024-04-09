3944e425e98fd80f9bc3f56b6466cd1d81e9a832
vixhnuchandran <vixhnuchandran@gmail.com>
2024-04-09 09:07:45 +0530

    Add entry point for 'cards' ui

---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Cards from "@/components/Cards/Cards"
+
+export default function CardsUI() {
+    return (
+        <>
+            <div className="flex-col  md:flex m-0 h-fit w-fit border rounded-lg ">
+                <Cards />
+            </div>
+        </>
+    )
+}
