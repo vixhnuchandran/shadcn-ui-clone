88852b4c1f403972dca7f71191d95339a8dd4b24
vixhnuchandran <vixhnuchandran@gmail.com>
2024-04-09 09:01:48 +0530

    Add Home page to route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { ThemeProvider } from "@/components/theme-provider"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
-import Home from "./pages/Home"
+import HomePage from "./pages/Home"
 
 function App() {
     const router = createBrowserRouter([
         {
             path: "/",
-            element: <Home />,
+            element: <HomePage />,
+            errorElement: <div className="text-4xl">404 not Found</div>,
         },
     ])
-
     return (
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
             <RouterProvider router={router} />
