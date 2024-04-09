f1d41e6694ba97baf64fb2aade9467386acb189d
vixhnuchandran <vixhnuchandran@gmail.com>
2024-04-09 09:12:08 +0530

    Add entry point for 'authentication' ui

---
+++ src/components/Authentication/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Authentication from "@/components/Authentication/Authentication"
+
+export default function AuthUI() {
+    return (
+        <>
+            <div className="flex-col  md:flex m-0 h-fit w-fit border rounded-lg ">
+                <Authentication />
+            </div>
+        </>
+    )
+}
