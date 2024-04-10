aed5acc62bde5579f6423ebfd2bb3060a69da40f
vixhnuchandran <vixhnuchandran@gmail.com>
2024-04-10 10:30:11 +0530

    Add playground ui components

---
+++ src/components/Playground/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Playground from "@/components/Playground/Playground"
+
+export default function PlaygroundUI() {
+  return (
+    <>
+      <div className="border rounded-lg ">
+        <Playground />
+      </div>
+    </>
+  )
+}
