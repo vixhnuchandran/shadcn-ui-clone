b286fb7adf108fdd3b745c6db6bfedb2a96b6347
vixhnuchandran <vixhnuchandran@gmail.com>
2024-04-10 09:09:19 +0530

    Add dashboard ui components

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Dashboard from "@/components/Dashboard/Dashboard"
+
+export default function DashboardUI() {
+  return (
+    <>
+      <div className="border rounded-lg ">
+        <Dashboard />
+      </div>
+    </>
+  )
+}
